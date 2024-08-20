ff477294b69d348ae9278894942ee0c5fbc6395a
Jay Basotia <153049713+psyschology@users.noreply.github.com>
2024-08-20 15:47:44 +0530

    Update admin.js

--- public/admin.js
+++ public/admin.js
@@ -121,10 +121,6 @@ bookTicketButton.addEventListener('click', () => {
 
 function generateBoardNumbers() {
     const board = Array.from({ length: 90 }, (_, i) => i + 1);
-    for (let i = board.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [board[i], board[j]] = [board[j], board[i]];
-    }
     return board;
 }
 
